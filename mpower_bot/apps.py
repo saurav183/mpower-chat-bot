09096c2af76ce8938b9487337638530d6e55aa97
saurav183 <saurav.kumar@mobicule.com>
2018-07-09 13:00:50 +0530

    my mpower bot app,first commit

---
+++ mpower_bot/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MpowerBotConfig(AppConfig):
+    name = 'mpower_bot'
---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/saurav/Documents/python_env/django_example/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
